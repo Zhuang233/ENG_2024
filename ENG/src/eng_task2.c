3294b531b75b7358835fde4e0cb32317f0984d0b
zbw <1552798761@qq.com>
2024-05-31 06:07:52 +0800

    Fshit F举着兑换

--- ENG/src/eng_task2.c
+++ ENG/src/eng_task2.c
@@ -730,28 +730,35 @@ void ModePoseTask(void const * argument){
 			
 			case SELECT_EXCANGE_MODE:{	
 				// …œŒ¸≈Ã∂“ªªF
-//				if(Key_Check_Hold(&Keys.KEY_SHIFT) &&  Key_Check_Press(&Keys.KEY_F)){
-//						*lift = -1400000;
-//						*qs = 0;
-//						*hy = -194951;
-//						*pitch = 28492;
-//						*roll = ARM_ANGLE_STD_2;
-//						*yaw = ARM_ANGLE_STD_3;
-//						key_1_last = custom_controller_data_t.key_1;	
-//						osDelay(2000);
-//					// ∂“ªª—≠ª∑					
-//					for(;;){
-//						RoboArm_RC_Ctrl_Fixed_Point(); 
-//						if(Key_Check_Hold(&Keys.KEY_CTRL) &&  Key_Check_Press(&Keys.KEY_S)) {
-//							pump_top_close();
-//							xipan_top_close();
-//							break; 
-//						}
-//						
-//						osDelay(1);
-//					}	
-//					posemod = NONE;
-//				}
+				if(Key_Check_Hold(&Keys.KEY_SHIFT) &&  Key_Check_Press(&Keys.KEY_F)){
+						*lift = -1400000;
+						*qs = 0;
+						*hy = -194951;
+						*pitch = 28492;
+						*roll = ARM_ANGLE_STD_2;
+						*yaw = ARM_ANGLE_STD_3;
+					
+					
+						*qs = 98042;
+						*hy = -162500;
+						*lift = -1009038;
+						*pitch = 28492;
+						*roll = 25823;
+						*yaw = 24370; 
+						key_1_last = custom_controller_data_t.key_1;	
+						osDelay(3500);
+						rotateslow_flag = false;
+					// ∂“ªª—≠ª∑					
+					for(;;){
+						RoboArm_RC_Ctrl_Fixed_Point(); 
+						if(Key_Check_Hold(&Keys.KEY_CTRL) &&  Key_Check_Press(&Keys.KEY_S)) {
+							rotateslow_flag = true;
+							break; 
+						}
+						osDelay(1);
+					}	
+					posemod = NONE;
+				}
 				
 				// ◊ÛŒ¸≈Ã∂“ªª C
 				if(Key_Check_Hold(&Keys.KEY_SHIFT) &&  Key_Check_Press(&Keys.KEY_C)){
@@ -850,6 +857,10 @@ void ModePoseTask(void const * argument){
 					}
 					posemod = NONE;
 				}
+				
+				
+				
+
 			} break;
 		}
 		osDelay(1);
